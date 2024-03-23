b50bde977bc047f6dc5b94a50b717a1e1d0311dd
Waqar Bin Abrar <31979294+waqaremadina1@users.noreply.github.com>
2024-03-23 12:13:48 +0500

    script.js

--- js/script.js
+++ js/script.js
@@ -2,37 +2,37 @@
 
 document.getElementById("some-string").onclick = function () {
 
-    let message = "Have a nice day"
+    let message = "Have a Nice Day"
     let userName = "Waqar"
     let banger = "!"
     let sum = (message + " " + userName + banger)
-    document.getElementById("statement").innerHTML = '<p>"Have a nice day" + " " + "Waqar" + "!"</p>'
+    document.getElementById("statement").innerHTML = '<p>"Have a Nice Day" + " " + "Waqar" + "!"</p>'
     document.getElementById("output").innerHTML = sum;
 }
 
 // Ask Name From User 
 
 document.getElementById("user").onclick = function(){
-    let firstName = prompt("please enter your first name")
-    let lastName = prompt("please enter your last name")
+    let firstName = prompt("Please Enter Your First Name")
+    let lastName = prompt("Please Enter Your Last Name")
     let fullName = `${firstName} ${lastName}`
-    document.getElementById("statement").innerHTML = `first name = ${firstName} <br> last name = ${lastName} <br> full name = first name + last name`
-    document.getElementById("output").innerHTML = `full name = ${fullName}`
+    document.getElementById("statement").innerHTML = `First Name = ${firstName} <br> Last Name = ${lastName} <br> Full Name = First Name + Last ame`
+    document.getElementById("output").innerHTML = `Full Name = ${fullName}`
 }
 
 // Comparison Operator 
 
 document.getElementById("operate").onclick = function (){
 
-    let num1 = +prompt("please enter first number")
-    let num2 = +prompt("please enter second number")
+    let num1 = +prompt("Please Enter First Number")
+    let num2 = +prompt("Please Enter Second Number")
     if (num1 === num2){
         document.getElementById("statement").innerHTML = "num1 == num2"
-        document.getElementById("output").innerHTML = "<span class='text-success'>Condition is true &#128522;<span/>"
+        document.getElementById("output").innerHTML = "<span class='text-success'>Condition is True &#128522;<span/>"
     }
     else {
         document.getElementById("statement").innerHTML = "num1 !== num2"
-        document.getElementById("output").innerHTML = "<span class='text-danger'>Condition is false &#128554;</span."
+        document.getElementById("output").innerHTML = "<span class='text-danger'>Condition is False &#128554;</span."
     }
 }
 
@@ -40,15 +40,15 @@ document.getElementById("operate").onclick = function (){
 
 document.getElementById("if-else").onclick = function (){
     document.getElementById("statement").innerHTML = " ";
-    let marks = +prompt("please enter your marks")
+    let marks = +prompt("Please Enter Your Marks")
     if (marks >= 90){
-        document.getElementById("output").innerHTML = "<span class='text-success'>Congratulations! you got A+ Grade</span> "
+        document.getElementById("output").innerHTML = "<span class='text-success'>Congratulations! You Got A+ Grade</span> "
     }
     else if (marks >=80){
-        document.getElementById("output").innerHTML = "<span class='text-success'>Congratulations! you got A Grade</span>"
+        document.getElementById("output").innerHTML = "<span class='text-success'>Congratulations! You Got A Grade</span>"
     }
     else if (marks >=70){
-        document.getElementById("output").innerHTML = "<span class='text-warning'>you got B Grade</span>"
+        document.getElementById("output").innerHTML = "<span class='text-warning'>You Got B Grade</span>"
     }
     else {
         document.getElementById("output").innerHTML = "<span class='text-danger'>You Are Fail. &#128554;</span>"
@@ -60,22 +60,22 @@ document.getElementById("if-else").onclick = function (){
 
 document.getElementById("test").onclick = function (){
     document.getElementById("statement").innerHTML = " ";
-    let age = prompt("please enter your age")
-    let weight = prompt("please enter your weight")
+    let age = prompt("Please Enter Your Age")
+    let weight = prompt("Please Enter Your Weight")
     if (age <= 10 && weight <= 25) {
-        document.getElementById("output").innerHTML = "you are a baby"
+        document.getElementById("output").innerHTML = "You are a Baby"
     }
     else if (age <= 18 && weight <= 50) {
-        document.getElementById("output").innerHTML = "you are teenager and healthy"
+        document.getElementById("output").innerHTML = "You Are Teenager and Healthy"
     }
     else if (age <= 25 && weight <= 60) {
-        document.getElementById("output").innerHTML = "you are a youngman and fatty"
+        document.getElementById("output").innerHTML = "You are a Youngman and Fatty"
     }
     else if (age <= 30 && weight <= 59) {
-        document.getElementById("output").innerHTML = "you are a youngman and healthy"
+        document.getElementById("output").innerHTML = "You are a Youngman and Healthy"
     }
     else {
-        document.getElementById("output").innerHTML = "you are a Senior Citizen and fatty"
+        document.getElementById("output").innerHTML = "You are a Senior Citizen and Fatty"
     }
 
 }
@@ -83,18 +83,18 @@ document.getElementById("test").onclick = function (){
 // If Statement Nested 
 
 document.getElementById("nest").onclick = function(){
-    let userName = prompt("please enter username")
+    let userName = prompt("Please Enter Username")
     if(userName === "Waqar"){
-        let password = prompt("please enter password")
+        let password = prompt("Please Enter Password")
         if(password == 123){
-            document.getElementById("output").innerHTML = "<span style='color:green; text-transform:capitalize;'>you are successfully logged in</span>"
+            document.getElementById("output").innerHTML = "<span style='color:green; text-transform:capitalize;'>You Are Successfully Logged In</span>"
         }
         else{
-            document.getElementById("output").innerHTML = `<span class='text-danger'>${password}</span> is incorrect password. <br> correct password is <span class='text-success'>123</span>`
+            document.getElementById("output").innerHTML = `<span class='text-danger'>${password}</span> is Incorrect Password. <br> Correct Password is <span class='text-success'>123</span>`
         }
     }
     else{
-        document.getElementById("output").innerHTML = `<span class='text-danger'>${userName} </span> is incorrect username. <br> correct user name is <span class='text-success'> "Waqar"</span>`
+        document.getElementById("output").innerHTML = `<span class='text-danger'>${userName} </span> is Incorrect Username. <br> Correct Username is <span class='text-success'> "Waqar"</span>`
     }
 }
 
@@ -103,14 +103,14 @@ document.getElementById("nest").onclick = function(){
 
 document.getElementById("login").onclick = function (){
     document.getElementById("statement").innerHTML = " ";
-    let userName = prompt("please enter username")
-    let passWord = +prompt("please enter password")
+    let userName = prompt("Please Enter Username")
+    let passWord = +prompt("Please Enter Password")
     if (userName === "Waqar" && passWord === 123 ){
         document.getElementById("form").innerHTML = `<span style='color:green; font-weight:bold;'>${userName}</span>`
-        document.getElementById("output").innerHTML = `<span style='color:green; text-transform:capitalize;'>you are successfully logged in</span>`
+        document.getElementById("output").innerHTML = `<span style='color:green; text-transform:capitalize;'>You Are Successfully Logged In</span>`
     }
     else {
-        document.getElementById("output").innerHTML = "Try using Waqar as username and 123 as a password"
+        document.getElementById("output").innerHTML = "Try Using Waqar as Username and 123 as a Password"
     }
 
 }
